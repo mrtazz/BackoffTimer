be507790f4d9ac0214b3a6242a22149492a0570b
Daniel Schauenberg <d@unwiredcouch.com>
2016-12-17 22:25:14 -0500

    turn BackoffTimer into a trait
    this allows for different backoff timers to be implemented with the same
    interface.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
 use std::time::Duration;
 use std::thread;
 
-/// BackoffTimer implements a timer that waits a certain amount and then exponentially backs off
+/// BackoffTimer implements a timer that waits a certain amount and then backs off
 /// until the configured max_wait_time (specified in seconds) is reached or passed.
 ///
-///
+/// Different specific timers implement their own version of back algorithms
+pub trait BackoffTimer {
+    fn wait(&mut self) -> u64;
+    fn is_done(&self) -> bool;
+}
+
 /// ```
 /// use backoff_timer::BackoffTimer;
+/// use backoff_timer::exponential_backoff_timer;
 ///
-/// let mut timer = BackoffTimer::new(5);
+/// let mut timer = exponential_backoff_timer(5);
 /// let mut waited = timer.wait();
 /// assert_eq!(waited, 2);
 /// assert_eq!(timer.is_done(), false);
@@ -16,27 +22,28 @@ use std::thread;
 /// assert_eq!(waited, 4);
 /// assert_eq!(timer.is_done(), true);
 /// ```
-pub struct BackoffTimer {
+pub struct ExponentialBackoffTimer {
     waited_time:   u64,
     wait_time:     u64,
     max_wait_time: u64,
 }
 
-impl BackoffTimer {
-
-    /// new returns a new BackoffTimer configured with the passed in max_wait_time in seconds
-    pub fn new(max_wait_time: u64) -> BackoffTimer {
-        BackoffTimer {
-            max_wait_time: max_wait_time,
-            wait_time: 2,
-            waited_time: 0,
-        }
+/// new returns a new ExponentialBackoffTimer configured with the passed in max_wait_time in seconds
+pub fn exponential_backoff_timer(max_wait_time: u64) -> ExponentialBackoffTimer {
+    ExponentialBackoffTimer {
+        max_wait_time: max_wait_time,
+        wait_time: 2,
+        waited_time: 0,
     }
+}
+
+
+impl BackoffTimer for ExponentialBackoffTimer {
 
     /// wait is a blocking function that waits for the appropriate amount of time depending on the
     /// timers internal state. It returns the time waited which is 0 if the timer has reached its
     /// max_wait_time
-    pub fn wait(&mut self) -> u64 {
+    fn wait(&mut self) -> u64 {
         if self.is_done() {
             return 0;
         }
@@ -50,7 +57,7 @@ impl BackoffTimer {
 
     /// is_done is a helper method to determine whether the timer is done or still has turns to
     /// wait.
-    pub fn is_done(&self) -> bool {
+    fn is_done(&self) -> bool {
         self.waited_time >= self.max_wait_time
     }
 
@@ -61,10 +68,11 @@ impl BackoffTimer {
 #[cfg(test)]
 mod tests {
     use super::BackoffTimer;
+    use super::exponential_backoff_timer;
 
     #[test]
     fn wait_for_5_seconds() {
-        let mut timer = BackoffTimer::new(5);
+        let mut timer = exponential_backoff_timer(5);
         let mut waited = timer.wait();
         assert_eq!(waited, 2);
         assert_eq!(timer.is_done(), false);
